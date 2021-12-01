27bbecd5c59844b3142804d6c2dadf1f12be7de5
Hsin-Yu Huang <brayce1996@gmail.com>
2021-11-30 18:41:13 -0600

    backup stored procedure

--- sql/init.sql
+++ sql/init.sql
@@ -129,3 +129,164 @@ CREATE TABLE `JOB_TAG` (
 );
 
 GRANT ALL PRIVILEGES ON joboss.* TO 'joboss'@'%';
+
+DELIMITER $$
+CREATE DEFINER=`root`@`%` PROCEDURE `recommend`(IN `my_job_id` INT, IN `my_user_id` INT)
+BEGIN
+
+  DECLARE cur CURSOR FOR 
+    SELECT Y.job_id as job_id
+    FROM (
+      SELECT job_id, similar_factor
+      FROM (SELECT js2.user_id as user_id, COUNT(*) as similar_factor
+            FROM JOB_STATUS as js JOIN JOB_STATUS as js2 ON (js.job_id = js2.job_id AND js.user_id <> js2.user_id)
+            WHERE js.user_id = my_user_id
+            GROUP BY js2.user_id
+            ORDER BY COUNT(*) DESC) as X 
+            JOIN 
+            JOB_STATUS as js ON X.user_id = js.user_id
+      GROUP BY job_id, similar_factor
+      ORDER BY similar_factor
+    ) as Y JOIN JOB as j ON (Y.job_id = j.id) JOIN COMPANY as c ON (j.company_id = c.id)
+    WHERE Y.job_id NOT IN (
+      SELECT DISTINCT myJs.job_id FROM JOB_STATUS as myJs WHERE user_id = my_user_id
+    );
+
+
+IF my_job_id > 0 THEN
+  SELECT jt.job_id as job_id, c.name as company_name, j.title as job_title
+  FROM JOB_TAG as jt, JOB as j, COMPANY as c
+  WHERE jt.tag_id IN (
+    SELECT tag_id
+    FROM JOB_TAG
+    WHERE job_id = my_job_id
+  ) AND jt.job_id != my_job_id
+  AND jt.job_id = j.id AND j.company_id = c.id
+  GROUP BY jt.job_id
+  HAVING COUNT(jt.tag_id) >= 1
+  ORDER BY COUNT(jt.tag_id) DESC
+  limit 10;
+ELSEIF my_user_id <= 0 THEN
+  (SELECT jt.job_id as job_id, c.name as company_name, j.title as job_title FROM `JOB_STATUS` as jt, `JOB` as j, `COMPANY` as c WHERE jt.job_id = j.id AND c.id = j.company_id GROUP BY jt.job_id ORDER BY count(1) DESC LIMIT 5)
+  UNION
+  (SELECT jt.job_id as job_id, c.name as company_name, j.title as job_title FROM `JOB_STATUS` as jt, `JOB` as j, `COMPANY` as c WHERE jt.job_id = j.id AND jt.job_id AND c.id = j.company_id GROUP BY jt.job_id ORDER BY count(jt.create_at) DESC LIMIT 5);
+ELSE
+  
+  OPEN cur;
+  BEGIN
+      DECLARE curr_job_id INT;
+      DECLARE exist_history BOOLEAN DEFAULT FALSE;
+      DECLARE exit_flag BOOLEAN DEFAULT FALSE;
+      DECLARE CONTINUE HANDLER FOR NOT FOUND SET exit_flag=True;
+
+      cloop: LOOP    
+      IF exit_flag THEN
+          LEAVE cloop;
+      END IF;
+      FETCH cur INTO curr_job_id;
+
+      SELECT COUNT(*) INTO exist_history FROM RECOMMEND_HISTORY as rh WHERE rh.job_id = curr_job_id;
+    	IF exist_history <= 0 THEN
+        INSERT IGNORE INTO RECOMMEND_HISTORY (job_id, recommend_count) VALUES (curr_job_id, 0);
+      END IF;
+      UPDATE RECOMMEND_HISTORY as rh SET recommend_count = recommend_count + 1 WHERE rh.job_id = curr_job_id;
+      END LOOP cloop;
+  END;
+  CLOSE cur;
+
+  SELECT Y.job_id as job_id, c.name as company_name, j.title as job_title
+  FROM (
+    SELECT job_id, similar_factor
+    FROM (SELECT js2.user_id as user_id, COUNT(*) as similar_factor
+          FROM JOB_STATUS as js JOIN JOB_STATUS as js2 ON (js.job_id = js2.job_id AND js.user_id <> js2.user_id)
+          WHERE js.user_id = my_user_id
+          GROUP BY js2.user_id
+          ORDER BY COUNT(*) DESC) as X 
+          JOIN 
+          JOB_STATUS as js ON X.user_id = js.user_id
+    GROUP BY job_id, similar_factor
+    ORDER BY similar_factor
+  ) as Y JOIN JOB as j ON (Y.job_id = j.id) JOIN COMPANY as c ON (j.company_id = c.id)
+  WHERE Y.job_id NOT IN (
+    SELECT DISTINCT myJs.job_id FROM JOB_STATUS as myJs WHERE user_id = my_user_id
+  );
+  
+  
+END IF;
+END$$
+DELIMITER ;
+
+
+
+
+
+DELIMITER $$
+CREATE DEFINER=`root`@`%` PROCEDURE `UpdateJobTag`(IN `tag_name` VARCHAR(250))
+BEGIN
+
+DECLARE job_id INT;
+DECLARE tag_id_var INT;
+DECLARE cur CURSOR FOR SELECT id FROM JOB WHERE LOWER(text_description) LIKE LOWER(CONCAT('% ', tag_name, ' %'));
+
+OPEN cur;
+
+BEGIN
+    DECLARE exit_flag BOOLEAN DEFAULT FALSE;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET exit_flag=True;
+    INSERT IGNORE INTO TAG (tag, tag_type) VALUES (tag_name, "skills");
+    SELECT id INTO tag_id_var FROM TAG WHERE tag = tag_name;
+    DELETE FROM JOB_TAG WHERE tag_id=tag_id_var;
+
+    cloop: LOOP    
+    IF exit_flag THEN
+        LEAVE cloop;
+    END IF;
+    FETCH cur INTO job_id;
+    INSERT IGNORE INTO JOB_TAG (job_id, tag_id) VALUES (job_id, tag_id_var);
+    END LOOP cloop;
+END;
+CLOSE cur;
+
+END$$
+DELIMITER ;
+
+
+
+
+
+
+
+DELIMITER $$
+CREATE DEFINER=`root`@`%` PROCEDURE `JobDescriptionHook`(IN `job_id_var` INT)
+BEGIN
+
+DECLARE tag_id_var INT;
+DECLARE tag_name_var VARCHAR(250);
+DECLARE tag_exist_in_jd INT;
+DECLARE cur CURSOR FOR SELECT jt.tag_id as tag_id, t.tag as tag_name FROM TAG as t JOIN JOB_TAG as jt ON t.id = jt.tag_id
+        WHERE jt.job_id = job_id_var;
+
+OPEN cur;
+
+BEGIN
+    DECLARE exit_flag BOOLEAN DEFAULT FALSE;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET exit_flag=True;
+
+    cloop: LOOP    
+    IF exit_flag THEN
+        LEAVE cloop;
+    END IF;
+    FETCH cur INTO tag_id_var, tag_name_var;
+
+    SELECT count(1) INTO tag_exist_in_jd FROM JOB WHERE LOWER(text_description) LIKE LOWER(CONCAT('% ', tag_name_var, ' %')) AND id = job_id_var;
+
+    IF tag_exist_in_jd < 1 THEN
+        DELETE FROM JOB_TAG WHERE job_id=job_id_var AND tag_id=tag_id_var;
+    END IF;    
+
+    END LOOP cloop;
+END;
+CLOSE cur;
+
+END$$
+DELIMITER ;
